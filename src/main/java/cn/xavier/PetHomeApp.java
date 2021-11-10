27f2da3f2a814f4f7617c13e2a34c1a425548512
xavier <xaviershui@gmail.com>
2021-11-11 00:02:09 +0800

    initialization

---
+++ src/main/java/cn/xavier/PetHomeApp.java
@@ -0,0 +1,15 @@
+package cn.xavier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Zheng-Wei Shui
+ * @date 11/10/2021
+ */
+@SpringBootApplication
+public class PetHomeApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PetHomeApp.class, args);
+    }
+}
